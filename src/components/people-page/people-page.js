70fa4f69698270879389caa97274d93ff7117e8d
annchapon <annchapon@gmail.com>
2021-08-31 17:40:52 +0300

    add Row element-property

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import ErrorIndicator from '../error-indicator';
 import ItemList from '../item-list/item-list';
 import PersonDetails from '../person-details/person-details';
+import Row from '../row';
 import SwapiService from '../../services/swapi-service';
 
 import './people-page.css';
@@ -32,18 +33,19 @@ export default class PeoplePage extends Component {
       return <ErrorIndicator />;
     }
 
+    const itemList = (
+      <ItemList 
+        onItemSelected={this.onPersonSelected} 
+        getData={this.swapiService.getAllPeople} 
+        renderItem={({name, gender, birthYear}) => `${name} (${gender}, ${birthYear})`} />      
+    );
+
+    const personDetails = (
+      <PersonDetails personId={this.state.selectedPerson} />
+    );
+
     return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList 
-            onItemSelected={this.onPersonSelected} 
-            getData={this.swapiService.getAllPeople} 
-            renderItem={({name, gender, birthYear}) => `${name} (${gender}, ${birthYear})`} />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={this.state.selectedPerson} />
-        </div>
-      </div>
+      <Row left={itemList} right={personDetails} />
     );
   }
 }
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -12,7 +12,9 @@ export default class SwapiService {
 
   getAllPeople = async () => {
     const res = await this.getResource(`/people/`);
-    return res.results.map(this._transformPerson);
+    return res.results
+      .map(this._transformPerson)
+      .slice(0, 5);
   };
 
   getPerson = async (id) => {
@@ -22,7 +24,9 @@ export default class SwapiService {
 
   getAllPlanets = async () => {
     const res = await this.getResource(`/planets/`);
-    return res.results.map(this._transformPlanet);    
+    return res.results
+      .map(this._transformPlanet)
+      .slice(0, 5);;    
   };
 
   getPlanet = async (id) => {
@@ -32,7 +36,9 @@ export default class SwapiService {
 
   getAllStarships = async () => {
     const res = await this.getResource(`/starships/`);
-    return res.results.map(this._transformStarship);
+    return res.results
+      .map(this._transformStarship)
+      .slice(0, 5);;
   };
 
   getStarship = async (id) => {
